e4c9cb3ec69c5efc3b19d14b42a6c2540fb8a033
soojin <kimsoojin@gimsujin-ui-MacBookPro.local>
2020-08-30 12:54:33 +0900

    2020/08/29

---
+++ BOJ_어른상어.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 21
+
+using namespace std;
+
+
+
+/*
+ 1. 아무 냄새가 없는 칸으로 이동
+ 2. 그런 칸이 없으면 자신의 냄새가 있는 칸의 방향으로 잡는다
+ 
+상어가 맨 처음에 보고 있는 방향은 입력으로 주어지고, 그 후에는 방금 이동한 방향이 보고 있는 방향이 된다.
+ 1, 2, 3, 4는 각각 위, 아래, 왼쪽, 오른쪽을 의미한다.
+ */
+
+struct SHARK{
+    int x,y,dir;
+    bool die;
+    
+};
+
+int dx[5] = {0,-1,1,0,0};
+int dy[5] = {0,0,0,-1,1};
+
+int N,M,K;
+int map[MAX][MAX];
+int tm[MAX][MAX]={0,};
+int idx[MAX][MAX]={0,};
+
+int priority[401][5][5]={0,};
+
+SHARK shark[401];
+
+
+void move()
+{
+    int tmp[MAX][MAX]={0,};
+    int tm_tmp[MAX][MAX]={0,};
+    
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(tm[i][j]!=0)    tm_tmp[i][j] = tm[i][j];
+            else    tm_tmp[i][j] = 0;
+        }
+    }
+    
+    
+    for(int i=1;i<=M;i++)
+    {
+        if(shark[i].die)    continue;
+        
+        int x = shark[i].x;
+        int y = shark[i].y;
+        int dir = shark[i].dir;
+        
+        int flag=0;
+        
+        for(int j=1;j<5;j++){
+            
+            int ndir = priority[i][dir][j];
+            int nx = x + dx[ndir];
+            int ny = y + dy[ndir];
+            
+            
+            
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N )    continue;
+            else if(idx[nx][ny] != 0 && tm_tmp[nx][ny] !=0){
+                continue;
+            }
+            else{
+                flag =1;
+
+                if(tmp[nx][ny] != 0){
+                    int num = tmp[nx][ny];
+                    if(num > i) {
+                        tmp[nx][ny]=i;
+                        shark[num].die = true;
+                    }
+                    else{
+                        shark[i].die=true;
+                        break;
+                    }
+                }
+                else    tmp[nx][ny]=i;
+                
+                shark[i].x = nx;
+                shark[i].y = ny;
+                shark[i].dir = ndir;
+                
+                tm[nx][ny] = K+1;
+                idx[nx][ny] = i;
+                
+                break;
+            }
+        }
+        
+        if(!flag)
+        {
+            for(int j=1;j<5;j++){
+                
+                int ndir = priority[i][dir][j];
+                int nx = x + dx[ndir];
+                int ny = y + dy[ndir];
+                
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N)    continue;
+                if(idx[nx][ny] == i){
+                    
+                    tmp[nx][ny] = i;
+                    
+                    shark[i].x = nx;
+                    shark[i].y = ny;
+                    shark[i].dir = ndir;
+                    
+                    tm[nx][ny] = K+1;
+                    idx[nx][ny] = i;
+                    
+                    break;
+                                        
+                }
+            }
+        }
+    }
+    
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            map[i][j] =tmp[i][j];
+        }
+    }
+    
+    
+    
+}
+
+void time(){
+    
+    for(int i=0;i<N;i++){
+        for(int j=0; j<N;j++){
+            if(tm[i][j] > 0){
+                tm[i][j]--;
+                if(!tm[i][j])   idx[i][j]=0;
+            }
+        }
+    }
+}
+
+bool check()
+{
+    int num=0;
+    for(int i=0;i<N;i++){
+        for(int j=0; j<N;j++){
+            if(map[i][j] > 0)   num++;
+        }
+    }
+    
+    if(num==1)  return  true;
+    return false;
+    
+}
+void solve()
+{
+    int cnt =0;
+    while(cnt <= 1000){
+
+        cnt++;
+        move();
+        time();
+
+        if(check()) break;
+    }
+    if(cnt > 1000)  cout << "-1";
+    else    cout << cnt;
+}
+int main(void)
+{
+
+    cin >> N >> M >> K;
+    
+     for(int i=0;i<N;i++){
+         for(int j=0;j<N;j++){
+             int num;
+             cin >>num;
+             if(num != 0){
+                 tm[i][j] = K;
+                 idx[i][j] = num;
+                 map[i][j] = num;
+
+                 shark[num].x = i;
+                 shark[num].y = j;
+             }
+         }
+     }
+    
+    int d;
+    for(int i=1;i<=M;i++){
+        cin >> d;
+        shark[i].dir = d;
+        shark[i].die = false;
+    }
+    
+    for(int i=1;i<=M;i++){
+        for(int j=1;j<5;j++){
+            for(int k=1;k<5;k++){
+                int num;
+                cin >> num;
+                priority[i][j][k] = num;
+                
+            }
+        }
+    }
+    solve();
+    
+    return 0;
+    
+}
+
