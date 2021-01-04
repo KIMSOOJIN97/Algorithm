2291f1c76d59e7d0295771846331799bf567d653
soojin <kimsoojin@gimsujin-ui-MacBookPro.local>
2021-01-05 02:43:37 +0900

    2021/01/05

--- BOJ_가스관.cpp
+++ BOJ_가스관.cpp
@@ -1,230 +1,118 @@
 #include <iostream>
-#include <queue>
 #define MAX 26
+
 using namespace std;
 
-struct pos{
+struct pos {
     int x,y;
 };
 
-int N,M;
-int map[MAX][MAX]={0,};
-int visited[MAX][MAX]={0,};
+char map[MAX][MAX]={0,};
 
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,1,0,-1};
+int R,C;
 
+int dx[4] ={-1,0,1,0};
+int dy[4] ={0,1,0,-1};
 
-pos start,END;
+pos start;
 
-void solve(int x,int y)
+void solve(int x, int y)
 {
-    int v[4]={0,};
+    cout << x+1 << " " << y+1 << " ";
+    bool dir[4] = {0,};
     
     for(int i=0;i<4;i++){
+        
         int nx = x+dx[i];
         int ny = y+dy[i];
         
-        if(nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny]==0)  continue;
+        if(nx<0 || ny< 0 || nx >= R || ny >= C )   continue;
         
-        if(i==0 && (map[nx][ny] == 1 || map[nx][ny] == 4 ||map[nx][ny] == 5||map[nx][ny] == 7))    v[0]=1;
-        else if(i==1 && (map[nx][ny] == 3 || map[nx][ny] == 4 ||map[nx][ny] == 6||map[nx][ny] == 7))    v[1]=1;
-        else if(i==2 && (map[nx][ny] == 2 || map[nx][ny] == 3 ||map[nx][ny] == 5||map[nx][ny] == 7))    v[2]=1;
-        else if(i==3 && (map[nx][ny] == 1 || map[nx][ny] == 2 ||map[nx][ny] == 6||map[nx][ny] == 7))    v[3]=1;
+        if(map[nx][ny] != '.'){
+            if(i == 0 && (map[nx][ny] == '1' || map[nx][ny] == '4' || map[nx][ny] == '|' || map[nx][ny] =='+'))  dir[i]=true;
+            else if(i == 1 && (map[nx][ny] == '3' || map[nx][ny] == '4' || map[nx][ny] == '-' || map[nx][ny] =='+'))  dir[i]=true;
+            else if(i == 2 && (map[nx][ny] == '2' || map[nx][ny] == '3' || map[nx][ny] == '|' || map[nx][ny] =='+'))  dir[i]=true;
+            else if(i == 3 && (map[nx][ny] == '1' || map[nx][ny] == '2' || map[nx][ny] == '-' || map[nx][ny] =='+'))  dir[i]=true;
 
-
+        }
     }
-    cout<<x+1 << " " << y+1 << " ";
-    
-    if(v[0] && v[1] && v[2] && v[3] )   cout << "+";
-    else if(v[0]==0 && v[1] && v[2]==0 && v[3] )   cout << "-";
-    else if(v[0] && v[1]==0 && v[2] && v[3]==0 )   cout << "|";
-    else if(v[0]==0 && v[1] && v[2] && v[3]==0 )   cout << "1";
-    else if(v[0] && v[1] && v[2]==0 && v[3]==0 )   cout << "2";
-    else if(v[0] && v[1]==0 && v[2]==0 && v[3] )   cout << "3";
-    else if(v[0]==0 && v[1]==0 && v[2] && v[3] )   cout << "4";
+    if(dir[0] && dir[1] && dir[2] && dir[3])    cout << "+";
+    else if(dir[1] && dir[2])   cout << "1";
+    else if(dir[0] && dir[1])   cout << "2";
+    else if(dir[0] && dir[3])   cout << "3";
+    else if(dir[2] && dir[3])   cout << "4";
+    else if(dir[0] && dir[2])   cout << "|";
+    else if(dir[1] && dir[3])   cout << "-";
     
     return ;
 }
 
-void BFS()
+void DFS(int x, int y, int dir)
 {
-    queue <pos> q;
-    q.push({start.x,start.y});
-    
-    
-    while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
-        
-        visited[x][y] = 1;
-        
-        q.pop();
-        
-        int nx, ny;
-        
-        if(map[x][y] == 1){
-            
-            for(int i=1;i<3;i++){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-                
-            }
-        }
-        else if(map[x][y] == 2){
-            
-            for(int i=0;i<2;i++){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-                
-            }
-        }
-        else if(map[x][y] == 3){
-            
-            for(int i=0;i<4;i+=3){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-            }
-        }
-        else if(map[x][y] == 4){
-            
-            for(int i=2;i<4;i++){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-                
-            }
-        }
-        else if(map[x][y] == 5){
-            
-            for(int i=0;i<3;i+=2){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-                
-            }
-        }
-        else if(map[x][y] == 6){
-            
-            for(int i=1;i<4;i+=2){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-                
-            }
-        }
-        
-        else if(map[x][y] == 7){
-            
-            for(int i=0;i<4;i++){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                
-                if(map[nx][ny] == 0){
-                    solve(nx,ny);
-                    return;
-                }
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-            }
-        }
-        else if(map[x][y] == 8){
-            
-            for(int i=0;i<4;i++){
-                nx = x+dx[i];
-                ny = y+dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] == 1)  continue;
-                visited[nx][ny] = 1;
-                q.push({nx,ny});
-            }
-        }
+    if(map[x][y] == '.'){
+        solve(x,y);
+        return ;
     }
     
+    int nx = x+dx[dir];
+    int ny = y+dy[dir];
+    
+    if(dir ==0 ){
+        if(map[nx][ny] == '|' || map[nx][ny] == '+')    dir = 0;
+        else if(map[nx][ny] =='1')    dir= 1;
+        else if(map[nx][ny] =='4') dir = 3;
+    }
+    else if(dir == 1 ){
+        if(map[nx][ny] == '-' || map[nx][ny] == '+')    dir = 1;
+        else if(map[nx][ny] =='3')    dir= 0;
+        else if(map[nx][ny] =='4') dir = 2;
+    }
+    else if(dir == 2){
+        if(map[nx][ny] == '|' || map[nx][ny] == '+')    dir = 2;
+        else if(map[nx][ny] =='2')    dir= 1;
+        else if(map[nx][ny] =='3') dir = 3;
+    }
+    else if(dir == 3){
+        if(map[nx][ny] == '-' || map[nx][ny] == '+')    dir = 3;
+        else if(map[nx][ny] =='1')    dir= 2;
+        else if(map[nx][ny] =='2') dir = 0;
+    }
+    DFS(nx,ny,dir);
+    return;
 }
 
 int main(void)
 {
-    cin >> N >> M;
-    
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            char ch;
-            cin >>ch;
-            
-            if(ch == '.')   map[i][j]=0;
-            else if(ch == '|')   map[i][j]=5;
-            else if(ch == '-')   map[i][j]=6;
-            else if(ch == '+')   map[i][j]=7;
-            else if(ch == '1')   map[i][j]=1;
-            else if(ch == '2')   map[i][j]=2;
-            else if(ch == '3')   map[i][j]=3;
-            else if(ch == '4')   map[i][j]=4;
-                      
-            else if(ch == 'M'){
-                      start={i,j};
-                      map[i][j] = 8;
-            }
-            else if(ch == 'Z'){
-                      END={i,j};
-                      map[i][j] = 9;
-            }
+
+    cin >> R>>C;
+    for (int i=0; i<R; i++){
+        for(int j=0;j<C;j++){
+            cin >> map[i][j];
+            if(map[i][j]== 'M')  start = {i,j};
         }
     }
     
-    BFS();
+    int Direction =0;
+    
+    for (int i = 0; i < 4; i++) {
+        int nx =start.x + dx[i];
+        int ny =start.y + dy[i];
+
+        if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+        if (map[nx][ny] != '.') {
+            if (nx ==start.x) {
+                if (ny == start.y - 1)  Direction = 3;
+                else if (ny == start.y+ 1)  Direction = 1;
+            }
+            else if (ny == start.y) {
+                if (nx == start.x - 1)  Direction = 0;
+                else if (nx ==start.x + 1)  Direction = 2;
+            }
+        }
+    }
+
+    DFS(start.x,start.y,Direction);
     return 0;
 }
+
+
