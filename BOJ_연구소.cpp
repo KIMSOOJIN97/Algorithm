f5266f24ae4be64861f9e0b6afcc3d11343e8425
soojin_kim2 <soojin_kim2@tmax.co.kr>
2021-04-26 01:33:10 +0900

    2021/04/25

--- BOJ_연구소.cpp
+++ BOJ_연구소.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <queue>
 #define MAX 8
 
 using namespace std;
@@ -10,104 +10,81 @@ struct pos{
 };
 
 int N,M;
-int map[MAX][MAX];
-
-int virus_x[64]={0,};
-int virus_y[64]={0,};
-int virus=0;
+int map[MAX][MAX]={0,};
+int res =0;
 
 int dx[4] = {-1,0,1,0};
 int dy[4] = {0,1,0,-1};
 
-int MAX_AREA = 0;
+vector <pos> blank;
+queue <pos> virus;
 
-vector <pos> empty;
 
 
-int BFS()
+void BFS()
 {
-    
-    int visited[MAX][MAX];
-    int x,y;
-    
+    int visited[MAX][MAX]={0,};
     for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++)
-            visited[i][j] = map[i][j];
+        for(int j=0;j<M;j++)    visited[i][j]= map[i][j];
     }
-    
 
-    int tmp_vir = virus;
+    queue <pos> q = virus;
     
-    for(int i=0;i<tmp_vir;i++){
-        x = virus_x[i];
-        y = virus_y[i];
-        for(int k=0;k<4;k++){
+    while(!q.empty())
+    {
+        int x= q.front().x;
+        int y= q.front().y;
+        q.pop();
+        
+        for(int i=0;i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx<0 || ny < 0 || nx >=N || ny >= M || visited[nx][ny] != 0 )    continue;
             
-            int nx = x+dx[k];
-            int ny = y+dy[k];
-                
-            if(nx < 0 || ny <0 || nx >= N || ny >= M || visited[nx][ny] != 0)   continue;
             visited[nx][ny] = -1;
-            virus_x[tmp_vir]=nx;
-            virus_y[tmp_vir]=ny;
-            tmp_vir++;
+            q.push({nx,ny});
         }
     }
     
-    
-    int cnt =0;
+    int cnt=0;
     for(int i=0;i<N;i++){
         for(int j=0;j<M;j++){
-            if(visited[i][j]==0)    cnt++;
+            if(visited[i][j]== 0)   cnt++;
         }
     }
-    
-    return cnt;
-    
+    if(cnt > res)   res = cnt;
+    return ;
 }
 
+void DFS(int cnt,int idx)
+{
+    if(cnt == 3)
+    {
+        BFS();
+        return ;
+    }
+    
+    for(int i=idx ; i<blank.size();i++)
+    {
+        pos tmp = blank[i];
+        map[tmp.x][tmp.y]=1;
+        DFS(cnt+1,i+1);
+        map[tmp.x][tmp.y]=0;
+    }
+}
 int main(void)
 {
-
     cin >> N >> M;
     for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            cin >> map[i][j];
-            
-            if(map[i][j] == 2) {
-                virus_x[virus] = i;
-                virus_y[virus] = j;
-                virus++;
-            }
-            else if(map[i][j]==0)   empty.push_back({i,j});
+        for(int j=0;j<M;j++)
+        {
+            cin >>map[i][j];
+            if(map[i][j]==0)    blank.push_back({i,j});
+            else if(map[i][j]==2)   virus.push({i,j});
         }
     }
-    
-    int size = empty.size();
-    
-    
-    for(int i=0;i<size-2;i++){
-        for(int j=i+1;j<size-1;j++){
-            for(int k=j+1;k<size;k++){
-                
-
-                map[empty[i].x][empty[i].y] = 1;
-                map[empty[j].x][empty[j].y] = 1;
-                map[empty[k].x][empty[k].y] = 1;
-                
-                int cnt = BFS();
-                if( MAX_AREA < cnt) MAX_AREA = cnt;
-
-                map[empty[i].x][empty[i].y] = 0;
-                map[empty[j].x][empty[j].y] = 0;
-                map[empty[k].x][empty[k].y] = 0;
-            }
-        }
-    }
-    
-    cout << MAX_AREA;
-
+    DFS(0,0);
+    cout << res;
     return 0;
-    
 }
 
